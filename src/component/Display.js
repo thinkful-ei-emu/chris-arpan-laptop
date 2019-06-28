a85386cf97c93c024eb643994ab378c29490aca9
Arpan Patel <adp8ke@gmail.com>
2019-06-28 13:46:43 -0400

    finised specSelect and cleaned up display.js

--- src/component/Display.js
+++ src/component/Display.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import PriceCalc from './component/PriceCalc'
-import SpecSelect from './component/SpecSelect'
+// import PriceCalc from '.PriceCalc'
+import SpecSelect from './SpecSelect'
 
 
 function Display(props) {
@@ -18,46 +18,10 @@ function Display(props) {
           .reduce((acc, curr) => acc + props.selected[curr].cost, 0);    
 
 
-    const features = Object.keys(props.features)
-          .map(key => {
-            const options = props.features[key].map((item, index) => {
-              console.log(props.selected[key].name);
-              console.log(item);
-
-              const selectedClass = item.name === props.selected[key].name ? 'feature__selected' : '';
-              
-              const featureClass = 'feature__option ' + selectedClass;  
-
-              return <li key={index} className="feature__item">
-                <div className={featureClass}
-                  
-                  onClick={e => {
-                      props.updateFeature(key, item);
-                    }}>
-                    { item.name }
-                    ({ new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
-                      .format(item.cost) })
-                </div>
-              </li>
-            });
-
-            return <div className="feature" key={key}>
-              <div className="feature__name">{key}</div>
-              <ul className="feature__list">
-                { options }
-              </ul>
-            </div>
-          });     
-
     return (
         <main>
 
-          <SpecSelect features = {props.features}/>
-          <PriceCalc summary = {} total ={}/>
-          <section className="main__form">
-            <h3>TECH SPECS AND CUSTOMIZATIONS</h3>
-            { features }
-          </section>
+          <SpecSelect selected={props.selected} features={props.features} updateFeature={(feature, newValue) => props.updateFeature(feature, newValue)} />
           <section className="main__summary">
             <h3>NEW GREENLEAF 2018</h3>
             {summary}
--- src/component/SpecSelect.js
+++ src/component/SpecSelect.js
@@ -1,15 +1,37 @@
 import React from 'react';
 
-function SpecSelect() {
-    const summary = Object.keys(props.selected)
-    .map(key => <div className="summary__option" key={key}>
-      <div className="summary__option__label">{key}  </div>
-      <div className="summary__option__value">{props.selected[key].name}</div>
-      <div className="summary__option__cost">
-        { new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
-            .format(props.selected[key].cost) }
-      </div>
-  </div>)
+function SpecSelect(props) {
+
+    const features = Object.keys(props.features)
+          .map(key => {
+            const options = props.features[key].map((item, index) => {
+              console.log(props.selected[key].name);
+              console.log(item);
+
+              const selectedClass = item.name === props.selected[key].name ? 'feature__selected' : '';
+              
+              const featureClass = 'feature__option ' + selectedClass;  
+
+              return <li key={index} className="feature__item">
+                <div className={featureClass}
+                  
+                  onClick={e => {
+                      props.updateFeature(key, item);
+                    }}>
+                    { item.name }
+                    ({ new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
+                      .format(item.cost) })
+                </div>
+              </li>
+            });
+
+            return <div className="feature" key={key}>
+              <div className="feature__name">{key}</div>
+              <ul className="feature__list">
+                { options }
+              </ul>
+            </div>
+          });     
 
     return (
         <section className="main__form">
@@ -20,4 +42,4 @@ function SpecSelect() {
 }
 
 
-export default SpecSelect
+export default SpecSelect;
